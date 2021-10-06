4c846c60cfb2939708d4f26be3805d89f2a3ba0f
sartain <alex.sartain@zuhlke.com>
2021-10-06 09:00:10 +0100

    first session

---
+++ app/src/test/java/com/example/tictactoe/TicTacToeBoardTest.kt
@@ -0,0 +1,75 @@
+package com.example.tictactoe
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+
+/**
+ * ToDo:
+ *   - Has 9 empty spaces: Done
+ *   - Play a 'X' or 'O' in an empty space:
+ *      -Can play 'X' at (0,0) first move:
+ *      -Cannot play 'O' at (0,0) second move:
+ *      -Can play 'O' at (2,2) second move:
+ *   - Alternate between 'X' and 'O' player:
+ *   - Shows either draw / victor:
+ *
+ */
+class TicTacToeBoardTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun `when created has nine empty spaces`() {
+        //given
+        val ticTacToeBoard = TicTacToeBoard()
+        //when:
+        //then:
+        assertEquals(9, ticTacToeBoard.emptySpaces)
+    }
+
+    @Test
+    fun `given empty board when play x at (0,0) then written to board`() {
+        //given:
+        val testSubject = TicTacToeBoard()
+        //when:
+        testSubject.playMoveAtPosition(0, 0)
+        //then:
+        (0..2).forEach { r ->
+            (0..2).forEach { c ->
+                if (r == 0 && c == 0) {
+                    assertEquals('X', testSubject.getCharacterAtPosition(0, 0))
+                } else {
+                    assertEquals(' ', testSubject.getCharacterAtPosition(r, c))
+                }
+            }
+        }
+
+    }
+
+    @Test
+    fun `given played 'x' at (0,0) then cannot play 'o' at (0,0)`() {
+        //given:
+        val testSubject = TicTacToeBoard()
+        //when:
+        testSubject.playMoveAtPosition(0, 0) //'X'
+        testSubject.playMoveAtPosition(0, 0) //'O'
+        //then:
+        assertEquals('X', testSubject.getCharacterAtPosition(0, 0))
+    }
+}
+
+class TicTacToeBoard {
+    val emptySpaces = 9
+
+    fun playMoveAtPosition(x: Int, y: Int) = Unit
+    fun getCharacterAtPosition(x: Int, y: Int) = 'X'
+}
