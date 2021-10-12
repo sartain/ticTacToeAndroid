a2a78024d80f101192ea1d99a35fe56be23fd0df
sartain <alex.sartain@zuhlke.com>
2021-10-12 17:04:35 +0100

    Completed alternating plays 'X' and 'O' with working tests

--- app/src/test/java/com/example/tictactoe/TicTacToeBoardTest.kt
+++ app/src/test/java/com/example/tictactoe/TicTacToeBoardTest.kt
@@ -15,10 +15,14 @@ import org.junit.Assert.*
  *   - Has 9 empty spaces: Done
  *   - Play a 'X' or 'O' in an empty space:
  *      -Can play 'X' at (0,0) first move: Done
- *      -Cannot play 'O' at (0,0) second move:
- *      -Can play 'O' at (2,2) second move:
+ *      -Cannot play 'O' at (0,0) second move: Done
+ *      -Can play 'O' at (2,2) second move: Done
  *   - Alternate between 'X' and 'O' player:
+ *      -Can play 'X' at (1,1) third move: Done
  *   - Shows either draw / victor:
+ *      -When board is not full and no same three characters in a row game is playable:
+ *      -When board is full and no same three characters in a row call game a draw:
+ *      -Three of same characters in a row call game a win:
  *
  */
 class TicTacToeBoardTest {
@@ -37,11 +41,11 @@ class TicTacToeBoardTest {
     }
 
     @Test
-    fun `given empty board when play x at (0,0) then written to board`() {
+    fun `given empty board when play x at (0,0) then written to board and board is still playable`() {
         //given:
         val testSubject = TicTacToeBoard()
         //when:
-        testSubject.playMoveAtPosition(0, 0)
+        val gameState = testSubject.playMoveAtPosition(0, 0)
         //then:
         (0..2).forEach { r ->
             (0..2).forEach { c ->
@@ -52,7 +56,7 @@ class TicTacToeBoardTest {
                 }
             }
         }
-
+        assertEquals("playable", gameState)
     }
 
     @Test
@@ -60,10 +64,12 @@ class TicTacToeBoardTest {
         //given:
         val testSubject = TicTacToeBoard()
         //when:
-        testSubject.playMoveAtPosition(0, 0) //'X'
-        testSubject.playMoveAtPosition(0, 0) //'O'
+        val result1 = testSubject.playMoveAtPosition(0, 0) //'X'
+        val result2 = testSubject.playMoveAtPosition(0, 0) //'O'
         //then:
         assertEquals('X', testSubject.getCharacterAtPosition(0, 0))
+        assertEquals("playable", result1)
+        assertEquals("playable", result2)
     }
 
     @Test
@@ -71,21 +77,54 @@ class TicTacToeBoardTest {
         //given:
         val testSubject = TicTacToeBoard()
         //when:
-        testSubject.playMoveAtPosition(0, 0) //'X'
-        testSubject.playMoveAtPosition(2, 2) //'O'
+        val result1 = testSubject.playMoveAtPosition(0, 0) //'X'
+        val result2 = testSubject.playMoveAtPosition(2, 2) //'O'
         //then:
         assertEquals('X', testSubject.getCharacterAtPosition(0,0))
         assertEquals('O', testSubject.getCharacterAtPosition(2,2))
+        assertEquals("playable", result1)
+        assertEquals("playable", result2)
     }
+
+    @Test
+    fun `alternate between 'X' and 'O'`() {
+        //given:
+        val testSubject = TicTacToeBoard()
+        //when:
+        val result1 = testSubject.playMoveAtPosition(0, 0) //'X'
+        val result2 = testSubject.playMoveAtPosition(2, 2) //'O'
+        val result3 = testSubject.playMoveAtPosition(1, 0) //'X'
+        val result4 = testSubject.playMoveAtPosition(0, 1) //'O'
+        //then:
+        assertEquals('X', testSubject.getCharacterAtPosition(0,0))
+        assertEquals('O', testSubject.getCharacterAtPosition(2,2))
+        assertEquals('X', testSubject.getCharacterAtPosition(1,0))
+        assertEquals('O', testSubject.getCharacterAtPosition(0,1))
+        assertEquals("playable", result1)
+        assertEquals("playable", result2)
+        assertEquals("playable", result3)
+        assertEquals("playable", result4)
+    }
+    //Test for not playable (draw result) FULL BOARD
 }
 
 class TicTacToeBoard {
-    val board = arrayOf(arrayOf(' ', ' ', ' '), arrayOf(' ', ' ', ' '), arrayOf(' ', ' ', ' '))
+    val board = arrayOf(arrayOf(BLANK, BLANK, BLANK), arrayOf(BLANK, BLANK, BLANK), arrayOf(BLANK, BLANK, BLANK))
     val emptySpaces = board[0].size + board[1].size + board[2].size
+    var playX = true
 
-    fun playMoveAtPosition(x: Int, y: Int) {
-        board[x][y] = 'X'
+    fun playMoveAtPosition(x: Int, y: Int) :String {
+        if(board[x][y] != BLANK) return "playable"
+        board[x][y] = if(playX) X else O
+        playX = !playX
+        return "playable"
     }
 
     fun getCharacterAtPosition(x: Int, y: Int) = board[x][y]
+
+    companion object{
+        private const val BLANK = ' '
+        private const val X = 'X'
+        private const val O = 'O'
+    }
 }
